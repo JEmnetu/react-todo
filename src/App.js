7aff65241109eac5f782a99adbaaef441f28bc83
JEmnetu <j.habtema@gmail.com>
2020-06-02 15:21:41 -0400

    styled todo list

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
+  const [todos, setTodos] = useState([{ text: "Todo", isCompleted: false }]);
+
   return (
     <div className="App">
-      <h1>Todo Application</h1>
+      <h1 id="main-heading">Todo Application</h1>
+      <div id="todo-list">
+        {todos.map((todo, index) => (
+          <p className="todo">{todo.text}</p>
+        ))}
+      </div>
     </div>
   );
 }
